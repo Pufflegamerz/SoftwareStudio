83a84567926654167ce372722ca9f642069efee2
Pufflegamerz <32369330+Pufflegamerz@users.noreply.github.com>
2017-12-28 18:25:50 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,16 @@
 function onclick(id,code) {
-	document.getElementById(id).addEventListener("click", code);
+	document.getElementById(id).setAttribute("onclick",code);
 }
+function onhover(id,code) {
+	document.getElementById(id).setAttribute("onhover",code);
+}
+function screenshot() {
+	// Use Html2Canvas
+	html2canvas(document.querySelector("#frame")).then(canvas => {
+		document.body.appendChild(canvas);
+		var screenshot = document.getElementsByTagName('canvas')[0].toDataURL("image/png");
+		document.getElementsByTagName('canvas')[0].outerHTML = "";
+		return screenshot
+	});
+}
+
